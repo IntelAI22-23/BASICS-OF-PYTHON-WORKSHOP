89fa18164bd8d219ec2ec262fb9cf5e0c243bdb0
Intel AI Students Club DYPCOE <110664932+IntelAI22-23@users.noreply.github.com>
2022-08-10 22:56:08 +0530

    Add files via upload

---
+++ Module 1 Introduction/Input_Output.py
@@ -0,0 +1,16 @@
+# Taking input from the user
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+########################################################
+
+# Integer input in Python
+# Taking input from the user as integer
+num = int(input("Enter a number: "))
+print(num)
+
+#########################################################
+
+# How to take Multiple Inputs in Python
+a, b, c = map(int, input("Enter the Numbers : ").split())
+print("The Numbers are : ", a, " ", b, " ", c)
---
+++ Module 1 Introduction/Printing_in_Python.py
@@ -0,0 +1,7 @@
+# Printing in Python
+print("Hello, World!")  # Printing a string
+print(1)  # Printing a integer
+print(1 + 2)  # Print the result
+print()  # Print blank line
+print("Hello", "World")  # Print two different strings
+
